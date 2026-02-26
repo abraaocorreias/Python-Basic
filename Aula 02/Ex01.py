1edffbf3daed6979655184cd3e463f3ede8080f9
abraaocorreias <abraaocorreias@gmail.com>
2026-02-26 10:39:02 -0300

    Add files via upload

---
+++ Aula 02/Ex01.py
@@ -0,0 +1,12 @@
+print("Olá, usuário!")
+a = float(input("Digite um número: "))
+b = float(input("Digite um outro número: "))
+
+soma = a+b
+sub = a-b
+prod = a*b
+divi = a/b
+
+
+print(f"Você digitou {a} e {b}")
+print(f"A soma destes números é {soma}, a subtração destes números é {sub}, a mutiplicação destes números é {prod} e a divisão entre eles é {divi}.")
---
+++ Aula 02/aula-02.py
@@ -0,0 +1,11 @@
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite outro número inteiro: "))
+
+soma = (a+b)
+divisão = (a/b)
+divint = (a//b)
+resto = (a%b)
+prod = (a*b)
+
+print(f"Você digitou {a} e {b}")
+print(f"A soma dos valores é {soma}, o produto destes valores é {prod}, a divisão exata é {divisão} e a divisão inteira é {divint}, com resto {resto}.")
